4edc3f9c59d43bd2ed0938ab1af2f43e96bf00dd
s-di-cola <admin@b-side.dev>
2025-07-31 12:15:00 +0100

    feat: add login

---
+++ src/auth.rs
@@ -0,0 +1,31 @@
+use std::io::{self, Write};
+
+pub struct Auth;
+
+impl Auth {
+    pub fn authenticate() -> Result<(), String> {
+        println!("=== Store Management System ===");
+        println!("Please login to continue");
+
+        let username = Self::read_input("Username:")?;
+        let password = Self::read_input("Password:")?;
+
+        if (username == "store_manager" && password == "pass1234") {
+            Ok(())
+        } else {
+            Err("Invalid username or password".to_string())
+        }
+    }
+
+    fn read_input(prompt: &str) -> Result<String, String> {
+        println!("{}", prompt);
+        io::stdout()
+            .flush()
+            .map_err(|_| "Failed to flush stdout. Please try again")?;
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .map_err(|err| err.to_string())?;
+        Ok(input.trim().to_string())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod inventory;
 mod purchase;
 mod sales;
 mod report;
+mod auth;
 
 fn main() {
     println!("Hello, world!");
